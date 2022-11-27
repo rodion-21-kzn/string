b5dbb32bee8e03748fd2ee12998e7f8326f0911d
oznakban <oznakban@student.21-school.ru>
2022-11-27 20:21:43 +0300

    start of the project

--- s21_string.c
+++ s21_string.c
@@ -2,20 +2,30 @@
 #include "s21_string.h"
 
 int main() {
-    char buf[200];
-    int x = 2;
-    sprintf(buf, "%100d", x);
-    printf("%s", buf);
+    char buf[10] = {NULL};
+    char *huk = NULL;
+    int i = s21_sprintf(buf, "Hello");
+    printf("error - %d\n", i);
+    printf("string - %s\n", buf);
     return 0;
 }
 
 int s21_sprintf(char *str, const char *format, ...) {
-    va_list(ap);
-    va_start(ap, format);
-
-
-
-
-
-    va_end(ap);
+    int err = 0;
+    if (format == NULL || *format == '\0') {  // в настоящем вроде сега
+        err = -1;
+    } else {
+        va_list(ap);
+        va_start(ap, format);
+        for (;*format; format++) {
+            if (*format != '%') { // если не процент, то сразу кидаем в массив
+                *str = *format;
+                str++;
+            } else {
+                return err;
+            }
+        }
+        va_end(ap);
+    }
+    return err;
 }
