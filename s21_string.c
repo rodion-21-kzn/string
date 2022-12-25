0ee888b338714dd88e714c2bdd5af3d807ed6fa9
r_shl <oznakban@student.21-school.ru>
2022-12-25 14:41:43 +0300

    parse width and flags

--- s21_string.c
+++ s21_string.c
@@ -1,14 +1,26 @@
 
 #include "s21_string.h"
 
+void print_token(format_token tok) {
+    printf("minus %d\n", tok.minus);
+    printf("plus %d\n", tok.plus);
+    printf("space %d\n", tok.space);
+    printf("zero %d\n", tok.zero);
+    printf("hash %d\n", tok.hash);
+    printf("length %c\n", tok.length);
+    printf("spec %c\n", tok.spec);
+    printf("prec %d\n", tok.prec);
+    printf("width %d\n", tok.width);
+}
+
 int main() {
-    char buf[10];
-    char *huk = NULL;
-    int i = s21_sprintf(buf, "Hello");
-    printf("error - %d\n", i);
-    printf("string - %s\n", buf);
+    char buf[100];
+    int x = 2;
+    s21_sprintf(buf, "AbaG%+012d", x);
+    printf("%s\n", buf);
     return 0;
 }
+// -0 не работаю вместе. Пробел и + не работают.
 
 // %[flags][width][.precision][length]specifier
 // flags (space, -, +, #, 0)
@@ -18,20 +30,71 @@ int main() {
 
 int s21_sprintf(char *str, const char *format, ...) {
     int err = 0;
+    format_token tok;
+    char *f = (char *)format;
+    init_format_token(&tok);
     if (format == NULL || *format == '\0') {  // в настоящем вроде сега
         err = -1;
     } else {
         va_list(ap);
         va_start(ap, format);
-        for (;*format; format++) {
-            if (*format != '%') { // если не процент, то сразу кидаем в массив
-                *str = *format;
+        for (;*f; f++) {
+            if (*f != '%') { // если не процент, то сразу кидаем в массив
+                *str = *f;
                 str++;
+                *str = '\0';
+            } else if (*f == '%') {
+                f++;
+                f = parse_flag(f, &tok);
+                f = parse_width(f, &tok, ap);
             } else {
                 return err;
             }
         }
         va_end(ap);
     }
+    print_token(tok);
     return err;
+}
+
+char* parse_width(char *f, format_token *tok, va_list ap) {
+    if (isdigit(*f)) {
+        char temp_buf[s21_buf_size];
+        int i = 0;
+        while (isdigit(*f)) {
+            temp_buf[i] = *f;
+            i++;
+            temp_buf[i] = '\0';
+            f++;
+        }
+        tok->width = atoi(temp_buf);
+    } else if (*f == '*') {
+        tok->width = va_arg(ap, int);
+        f++;
+    }
+    return f;
+}
+
+char* parse_flag(char *f, format_token *tok) {
+    while (*f == ' ' || *f == '+' || *f == '-' || *f == '0' || *f == '#') {
+        if (*f == ' ') tok->space = 1;
+        if (*f == '+') tok->plus = 1;
+        if (*f == '-') tok->minus = 1;
+        if (*f == '0') tok->zero = 1;
+        if (*f == '#') tok->hash = 1;
+        f++;
+    }
+    return f;
+}
+
+void init_format_token(format_token *tok) {
+    tok->minus = 0;
+    tok->plus = 0;
+    tok->space = 0;
+    tok->zero = 0;
+    tok->hash = 0;
+    tok->length = 'n';
+    tok->spec = 'n';
+    tok->prec = -1;
+    tok->width = -1;
 }
--- s21_string.h
+++ s21_string.h
@@ -5,15 +5,35 @@
 #ifndef STRING_S21_STRING_H
 #define STRING_S21_STRING_H
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <math.h>
+#include <ctype.h>
 
 #define s21_size_t unsigned long long
-#define s21_NULL (void*)0
+#define s21_NULL (void*)
+#define s21_buf_size 100
 
+typedef struct format_token {
+    int minus;
+    int plus;
+    int space;
+    int zero;
+    int hash;
+    int width;
+    int prec;
+    char length;
+    char spec;
+}format_token;
 
 int s21_sprintf(char *str, const char *format, ...);
+void init_format_token(format_token *tok);
+void parse_length(char **f, format_token *tok);
+char* parse_flag(char *f, format_token *tok);
+void parse_prec(char **f, format_token *tok, va_list args);
+char* parse_width(char *f, format_token *tok, va_list args);
+void parse_spec(char **f, format_token *tok);
 
 #endif //STRING_S21_STRING_H
